50e28737bcc17d95c6cd883d0531fdd9b857fd3b
PastelWhale <aviate8@gmail.com>
2021-08-04 23:09:45 +0900

    Add a contract to vote yes or no

---
+++ voting2.sol
@@ -0,0 +1,57 @@
+pragma solidity 0.8.6;
+contract Vote {
+    address owner;
+    enum VoteTypes { Notvoted, Yes, No }
+    mapping (address => VoteTypes) public votes;
+    address[] public voters;
+    string public question;
+    modifier onlyOwner() {require(msg.sender == owner); _;}
+    
+    constructor(string memory _question) public{
+        question = _question;
+        owner=msg.sender;
+        votes[0x0000000000000000000000000000000000000000]=VoteTypes.Notvoted;
+    }
+    
+    function voteYes() public returns (bool success) {
+        if(!addressInArray(msg.sender)){voters.push(msg.sender);}
+        votes[msg.sender] = VoteTypes.Yes;
+        return true;
+    }
+    
+    function voteNo() public returns (bool sucess) {
+        if(!addressInArray(msg.sender)){voters.push(msg.sender);}
+        votes[msg.sender] = VoteTypes.No;
+        return true;
+    }
+    
+    function removeVote() public {
+        require (addressInArray(msg.sender)); 
+        votes[msg.sender] = VoteTypes.Notvoted;
+        for(uint i=0; i<voters.length; i++){
+         if(voters[i] == msg.sender){
+            voters[i] = 0x0000000000000000000000000000000000000000;
+            }
+        }
+    }
+    
+    function results() public view returns (uint yesVotes, uint noVotes){
+        
+    }
+    
+     function getVoters() public view returns(address[] memory) {
+    return voters;
+    }
+    
+    
+    function addressInArray(address inAddress) private view returns (bool inArray) {
+        for(uint i=0; i<voters.length; i++){
+            if(voters[i] == inAddress){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+}
+
