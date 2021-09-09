6fb83c4391a443fe2e8322ab9ea92ac4fdfd0610
PastelWhale <43264739+HongDaeEui@users.noreply.github.com>
2021-09-09 22:38:22 +0900

    Modify: donate function

--- nextree.sol
+++ nextree.sol
@@ -8,6 +8,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 contract TreeItems is ERC1155 {
     
     mapping(address => uint) public balances;
+    mapping(address => uint) public drawcode;
     address public owner;
 
     uint256 public constant Baby = 0;
@@ -42,17 +43,18 @@ contract TreeItems is ERC1155 {
 }
 
 
+
      function donate() public payable {
         
         balances[msg.sender] += msg.value;
-        uint treeNumber; 
+        
         uint silverNumber;
         uint bronzeNumber;
         uint ironNumber;
         uint babyNumber;
+        uint treeNumber; 
         
-        
-        treeNumber = msg.value/5*10**14;
+        treeNumber = msg.value/(5*(10**14));
         
        
         if(treeNumber<10) {
@@ -71,20 +73,31 @@ contract TreeItems is ERC1155 {
              _safeTransferFrom(owner, msg.sender, 0, babyNumber, "");
               _safeTransferFrom(owner, msg.sender, 1, ironNumber, "");
                _safeTransferFrom(owner, msg.sender, 2, bronzeNumber, "");
-        }else if (treeNumber<1000) {
+        }else {
             silverNumber = treeNumber/200;
             bronzeNumber = (treeNumber%200)/50;
             ironNumber = (treeNumber%200)%50/10;
-            babyNumber = (treeNumber%200)
+            babyNumber = (treeNumber%200)%50%10;
             
-        }else {
+            _safeTransferFrom(owner, msg.sender, 0, babyNumber, "");
+              _safeTransferFrom(owner, msg.sender, 1, ironNumber, "");
+               _safeTransferFrom(owner, msg.sender, 2, bronzeNumber, "");
+               _safeTransferFrom(owner, msg.sender, 3, silverNumber, "");
+        }
+        
+        
+        if( balances[msg.sender] % 5*10**17 == 0) {
             
+            uint drawNumber;
+            drawNumber = balances[msg.sender]/(5*(10**17)) - drawcode[msg.sender];
+             _safeTransferFrom(owner, msg.sender, 7, drawNumber, "");
+             drawcode[msg.sender] += drawNumber;
         }
      }
      
      
-      function merge_cards(uint fromID, uint toID) public  {
         
+      function merge_cards(uint fromID, uint toID) public  {
         _safeTransferFrom(msg.sender, owner, fromID, 10, "");
         _safeTransferFrom(owner, msg.sender, toID, 1, "");
      }
